9389d8ef5fbcd90267c237820e75aac9fb2ef979
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2022-04-25 23:32:11 -0300

    Deletando um objeto do banco de dados com remove

--- src/main/java/aplication/Program.java
+++ src/main/java/aplication/Program.java
@@ -15,7 +15,9 @@ public class Program {
 		
 		Pessoa p = em.find(Pessoa.class, 2);
 		
-		System.out.println(p);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
 		
 		System.out.println("Pronto!");
 		em.close();
