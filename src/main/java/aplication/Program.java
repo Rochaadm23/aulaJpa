d623eaaa81dd9335ead739914031e78b67b1bb3e
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2022-04-25 23:26:25 -0300

    Introduzido o modo de busca de objetos ao banco de dados

--- src/main/java/aplication/Program.java
+++ src/main/java/aplication/Program.java
@@ -10,23 +10,17 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos.silva@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim.torres@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana.maria@gmail.com");
-
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("curso-jpa");
 		EntityManager em = emf.createEntityManager();
-
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
-
-		System.out.println("Pronto");
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		System.out.println(p);
+		
+		System.out.println("Pronto!");
 		em.close();
 		emf.close();
-		
+
 	}
 
 }
